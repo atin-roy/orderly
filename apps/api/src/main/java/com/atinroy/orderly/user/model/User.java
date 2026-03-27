0cabbed7b375a3b5f876540fe7c566205b476346
Atin Roy <me@atinroy.com>
2026-03-27 22:32:58 +0530

    Add User model class

--- apps/api/src/main/java/com/atinroy/orderly/user/model/User.java
+++ apps/api/src/main/java/com/atinroy/orderly/user/model/User.java
@@ -1,4 +1,20 @@
 package com.atinroy.orderly.user.model;
 
-public class User {
+import com.atinroy.orderly.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_user_keycloak_id", columnList = "keycloakId", unique = true)
+        }
+)
+public class User extends BaseEntity {
+    @Column(nullable = false, unique = true)
+    private String keycloakId;
 }
